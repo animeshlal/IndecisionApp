28df51899a74949991aa4a0b596c67ac02231e03
Animesh Lal <mmt6919@gmail.com>
2018-01-13 14:43:23 +0530

    Final initial Sync

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,61 +1,91 @@
 'use strict';
 
-// const square = function(x){
-//     return x*x;
-// }
+console.log('app.js is running!');
 
-// // const squareArrow = (x) => {
-// //     return x*x;
-// // }
+var app = {
+    title: 'Indecision App',
+    subtitle: 'Put your life in the hands of a computer',
+    options: []
+};
 
-// const squareArrow = (x) => x*x;
+var onFormSubmit = function onFormSubmit(e) {
+    e.preventDefault();
 
-// console.log(squareArrow(7));
+    var option = e.target.elements.option.value;
 
-// const getFirstName = (fullName) => {
-//     return fullName.split(' ')[0];
-// }
-
-// console.log(getFirstName('Animesh Lal'));
-
-// const getFirstNameShortHand = (fullName) => fullName.split(' ')[0];
-
-// console.log(getFirstNameShortHand('Abhishek Lal'));
-
-// const user = {
-//     name: 'Animesh',
-//     cities: ['Hazaribag','Ranchi','Bengaluru'],
-//     printPlacesLived(){
-//         this.cities.forEach((city) => {
-//             console.log(this.name + ' has lived in ' + city);
-//         });
-//     }
-// };
-
-var user = {
-    name: 'Animesh',
-    cities: ['Hazaribag', 'Ranchi', 'Bengaluru'],
-    printPlacesLived: function printPlacesLived() {
-        var _this = this;
-
-        return this.cities.map(function (city) {
-            return _this.name + ' has lived in ' + city;
-        });
+    if (option) {
+        app.options.push(option);
+        e.target.elements.option.value = '';
+        renderApp();
     }
 };
 
-// console.log(user.printPlacesLived());
-
-var multiplier = {
-    numbers: [1, 2, 3],
-    multiplyBy: 2,
-    multiply: function multiply() {
-        var _this2 = this;
-
-        return this.numbers.map(function (num) {
-            return _this2.multiplyBy * num;
-        });
-    }
+var removeAll = function removeAll() {
+    app.options = [];
+    renderApp();
 };
 
-console.log(multiplier.multiply());
+var onMakeDecision = function onMakeDecision() {
+    var randomNum = Math.floor(Math.random() * app.options.length);
+    var option = app.options[randomNum];
+    alert(option);
+};
+
+var appRoot = document.getElementById('app');
+
+var renderApp = function renderApp() {
+    var template = React.createElement(
+        'div',
+        null,
+        React.createElement(
+            'h1',
+            null,
+            app.title
+        ),
+        app.subtitle && React.createElement(
+            'p',
+            null,
+            app.subtitle
+        ),
+        React.createElement(
+            'p',
+            null,
+            app.options.length > 0 ? 'Here are your options' : 'No options'
+        ),
+        React.createElement(
+            'button',
+            { disabled: app.options.length === 0, onClick: onMakeDecision },
+            'What should I do?'
+        ),
+        React.createElement(
+            'button',
+            { onClick: removeAll },
+            'Remove All'
+        ),
+        React.createElement(
+            'ol',
+            null,
+            app.options.map(function (option) {
+                return React.createElement(
+                    'li',
+                    { key: option },
+                    option
+                );
+            })
+        ),
+        React.createElement(
+            'form',
+            { onSubmit: onFormSubmit },
+            React.createElement('input', { type: 'text', name: 'option' }),
+            React.createElement(
+                'button',
+                null,
+                'Add Option'
+            )
+        )
+    );
+
+    ReactDOM.render(template, appRoot);
+};
+
+renderApp();
