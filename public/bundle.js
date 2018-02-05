b400be8a698493e127879a499be270655f5c3bfb
Animesh Lal <mmt6919@gmail.com>
2018-02-05 23:14:45 +0530

    5/2/18 23:14

--- public/bundle.js
+++ public/bundle.js
@@ -65,9 +65,34 @@
 /************************************************************************/
 /******/ ([
 /* 0 */
-/***/ (function(module, exports) {
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
 
-console.log('app.js is running!');
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__person_js__ = __webpack_require__(2);
+// import { square } from './utils.js';
+
+// console.log('app.js is running!');
+// console.log(square(4));
+
+
+
+console.log(__WEBPACK_IMPORTED_MODULE_0__person_js__["b" /* isAdult */](17));
+console.log(__WEBPACK_IMPORTED_MODULE_0__person_js__["a" /* canDrink */](23));
+
+/***/ }),
+/* 1 */,
+/* 2 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+const isAdult = (x) => x>=18?true:false;
+/* harmony export (immutable) */ __webpack_exports__["b"] = isAdult;
+
+
+const canDrink = (x) => x>=21?true:false;
+/* harmony export (immutable) */ __webpack_exports__["a"] = canDrink;
+
 
 /***/ })
 /******/ ]);
--- src/app.js
+++ src/app.js
@@ -1,1 +1,9 @@
-console.log('app.js is running!');
+// import { square } from './utils.js';
+
+// console.log('app.js is running!');
+// console.log(square(4));
+
+import { isAdult, canDrink } from './person.js';
+
+console.log(isAdult(17));
+console.log(canDrink(23));
